b2e9206ea843d4d8618029e2f11802704ff2424f
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2022-12-17 20:11:04 +0100

    added missing page type 'about'

--- ndr_core/admin_views/page_views.py
+++ ndr_core/admin_views/page_views.py
@@ -100,6 +100,9 @@ class PageCreateView(LoginRequiredMixin, CreateView):
             elif self.object.page_type == self.object.PageType.FLIP_BOOK:
                 base_file = finders.find('ndr_core/app_init/flip_book.html')
                 shutil.copyfile(base_file, new_filename)
+            elif self.object.page_type == self.object.PageType.ABOUT_PAGE:
+                base_file = finders.find('ndr_core/app_init/about.html')
+                shutil.copyfile(base_file, new_filename)
         return response
 
 
