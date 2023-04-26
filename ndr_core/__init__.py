2a173086a489cb28c360ebd8f50ad21b188311be
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2023-04-26 15:40:20 +0200

    Changed setup and bumped version number

---
+++ ndr_core/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.4.0'
+""" NDR Core Version. Must be iterated when a new version is released. First number are major releases with 
+fundamental changes, second number are minor releases like new features, third number are bugfixes or added
+API implementations."""
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
-from ndr_core.ndr_settings import NdrSettings
+import ndr_core
 
-setup(version=NdrSettings.VERSION)
+setup(version=ndr_core.__version__)
