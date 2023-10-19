bd70d4eb0cbdbd8e657e6d6a08bef4763d852849
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2023-10-19 09:49:32 +0200

    Added locale middleware

--- django_ndr_core/settings.py
+++ django_ndr_core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS += NdrSettings.get_installed_apps()
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
