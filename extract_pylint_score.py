81fcfa7e1b4bfc432402ece831056fd391b51d21
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2023-11-12 20:28:30 +0100

    Pylint integration

---
+++ extract_pylint_score.py
@@ -0,0 +1,35 @@
+import re
+
+
+def extract_pylint_score(file_path):
+    """Extracts the pylint score from the output file"""
+    try:
+        with open(file_path, 'r', encoding='utf8') as file:
+            content = file.read()
+            # Regex to match the pylint score pattern
+            match = re.search(r'rated at ([0-9.]+)/10', content)
+            if match:
+                return match.group(1)  # Returns the captured score
+            else:
+                return "Score not found"
+    except FileNotFoundError:
+        return "File not found"
+
+
+def update_readme_with_badge(score, readme_file_path='README.md'):
+    badge_url = f"https://mperlet.github.io/pybadge/badges/{score}.svg"
+    badge_md = f"![pylint score]({badge_url})"
+
+    with open(readme_file_path, 'r') as file:
+        readme_content = file.read()
+
+    # Assuming you have a placeholder text in your README like: ![pylint score](old_url)
+    updated_content = re.sub(r'!\[pylint score]\(.*\)', badge_md, readme_content)
+
+    with open(readme_file_path, 'w') as file:
+        file.write(updated_content)
+
+
+if __name__ == "__main__":
+    score = extract_pylint_score('output.txt')
+    update_readme_with_badge(score)
