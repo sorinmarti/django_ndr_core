82e6adb8ee1e15d446656e3b939c8eca9cbc56b1
sorin <sorin.marti@gmail.com>
2026-05-28 11:05:13 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from pathlib import Path
 
 from setuptools import setup
-import os
-import json
 
-with open(os.path.join(Path(__file__).resolve().parent, 'info.json')) as info_file:
-    try:
-        data = json.load(info_file)
-        version = data['version']
-    except json.JSONDecodeError:
-        version = '-0.0.1'
-    except KeyError:
-        version = '-0.0.2'
+version_file = Path(__file__).resolve().parent / 'ndr_core' / 'VERSION'
+try:
+    version = version_file.read_text().strip()
+except FileNotFoundError:
+    version = '0.0.0'
 
 setup(version=version)
